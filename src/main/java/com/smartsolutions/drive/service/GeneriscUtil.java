0d8d1e309fc820b5132ca87d9a2bb9ad29480d80
HelmyJ <helmy.jeddi@gmail.com>
2021-02-05 15:25:08 +0100

    Change name package

--- src/main/java/com/smartsolutions/drive/service/GeneriscUtil.java
+++ src/main/java/com/smartsolutions/drive/service/GeneriscUtil.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.samrtsolutions.drive.service;
+package com.smartsolutions.drive.service;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
