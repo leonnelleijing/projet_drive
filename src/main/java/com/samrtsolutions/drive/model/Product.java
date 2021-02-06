89ace47317aa372c156d2f2a7de52f54c55de1aa
leonnelleijing <leonleijing@gmail.com>
2021-02-06 13:16:48 +0100

    update product test model

--- src/main/java/com/samrtsolutions/drive/model/Product.java
+++ src/main/java/com/samrtsolutions/drive/model/Product.java
@@ -14,8 +14,11 @@ import javax.persistence.Table;
 /**
  *
  * @author leonl
+ * poue tester, il faut juste dans bd. HibernateUtils, ajoute ma class et puis aller dans test package,
+ * run la méthode init(), après voir dans la bd si ma table est bien crée
  */
 @Entity
+//utiliser @table pour spécifier le nom de notre table dans la BD
 @Table(name="drive_produit")
 public class Product {
     private int productCode; 
@@ -29,6 +32,7 @@ public class Product {
     
     @Id
     @GeneratedValue
+    //utiliser @column pour spécifier le nom de column 
     @Column(name="ProductCode")
     public int getProductCode() {
         return productCode;
--- src/main/java/com/samrtsolutions/drive/service/ProductDao.java
+++ src/main/java/com/samrtsolutions/drive/service/ProductDao.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package metier;
+package com.samrtsolutions.drive.service;
 
 /**
  *
- * @author Nicol
+ * @author leonl
  */
-public class Produit {
+public class ProductDao {
     
 }
