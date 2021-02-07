0c58cbd9b6456b708477556cd8b920e750f2c11c
Nicolas <Nicolas.daoheuang@gmail.com>
2021-02-07 09:26:12 +0100

    US0.1-Afficher produit

---
+++ src/main/java/com/samrtsolutions/drive/repository/BaseDao.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.samrtsolutions.drive.repository;
+
+/**
+ *
+ * @author leonl
+ * @param <T>
+ */
+public interface BaseDao<T> {
+    public void edit(Object obj);        
+    public void delete(int id);            
+    public T load(int id);                
+    public T get(int id);              
+}
---
+++ src/main/java/com/samrtsolutions/drive/repository/BaseDaoImpl.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.samrtsolutions.drive.repository;
+import com.samrtsolutions.drive.bd.HibernateUtil;
+import javax.annotation.Resource;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author leonl
+ * @param <T>
+ */
+public class BaseDaoImpl<T> implements BaseDao<T> {
+
+@Resource
+    
+    private SessionFactory factory= HibernateUtil.getSessionFactory();
+    private Class<T> clazz = GeneriscUtil.getGenericType(this.getClass());
+    private Transaction t ;
+    
+    protected Session getSession() {
+        t=factory.getCurrentSession().beginTransaction();
+        return factory.getCurrentSession();
+    }
+    
+    public void edit(Object obj) {
+        getSession().saveOrUpdate(obj);
+        t.commit();
+         
+    }
+
+    public void delete(int id) {
+        Object object = getSession().get(clazz, id);
+        if(object != null) {
+            getSession().delete(object);
+            t.commit();
+        }
+    }
+
+    public T load(int id) {
+        return (T) getSession().load(clazz, id);
+    }
+
+    public T get(int id) {
+        return (T) getSession().get(clazz, id);
+    }
+
+    
+}
